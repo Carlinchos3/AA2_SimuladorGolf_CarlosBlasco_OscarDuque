b4c4327beea1ead1c999bb1cebdb021217bb6c64
Carlinchos <carlos.blasco@estudiant.enti.cat>
2026-05-18 23:46:44 +0200

    mas cambios

--- Assets/Proyecto/Scripts/LevelLoader.cs
+++ Assets/Proyecto/Scripts/LevelLoader.cs
@@ -3,40 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    //Singleton
-    public static LevelLoader Instance;
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Escape)) 
+        if (Input.GetKeyUp(KeyCode.Escape))
         {
             SceneManager.LoadScene("Menu");
         }
     }
 
-    public void GoToLevel1() 
-    { 
+    public void GoToLevel1()
+    {
         SceneManager.LoadScene("Level1");
     }
-    public void GoToLevel2() 
+
+    public void GoToLevel2()
     {
         SceneManager.LoadScene("Level2");
     }
-    public void GoToLevel3() 
+
+    public void GoToLevel3()
     {
         SceneManager.LoadScene("Level3");
     }
-
 }
